7482f713e689a52ebc45596a71e9da23e95230bb
onkar sinha <sinhaonkar234@gmail.com>
2024-04-16 19:45:55 +0530

    rece

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -26,7 +26,7 @@ const Services = () => {
                     onMouseEnter={() => setHoveredIndex(2)}
                     onMouseLeave={() => setHoveredIndex(null)}
                 >
-                    Online Cybersecurity Course
+                    Ethical Hacking Courses
                 </div>
                 <div
                     className={`text-lg font-semibold text-neutral-200 rounded-lg h-40 w-full md:w-72 border-2 border-green-600 flex items-center justify-center b_glow my-4 md:my-0 mx-4 md:mx-8 px-4 ${hoveredIndex === 3 ? 'hover:scale-105' : ''}`}
